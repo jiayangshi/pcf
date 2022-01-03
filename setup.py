61f63619b4994e10a637ef2b8d3cb3f793ebdbd6
Jiayang <shijiayang@gmx.de>
2022-01-03 21:10:34 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-setup(name='pcf',
+setup(python_requires='>=3.7',
+      name='pcf',
       version='0.0.1',
       description='The common used function with pytorch',
       url='https://github.com/jiayangshi/pcf',
@@ -8,4 +9,10 @@ setup(name='pcf',
       author_email='j.shi@liacs.leidenuniv.nl',
       license='MIT',
       packages=['pcf'],
+      install_requires=[
+          'pytorch >= 1.8.0',
+          'numpy',
+          'matplotlib',
+          'PIL'
+      ],
       zip_safe=False)
