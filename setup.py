00f53a53737b089057f204596dd32d726358c970
Jiayang <shijiayang@gmx.de>
2022-01-03 20:19:22 +0100

    make the project installable with pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pcf',
+      version='0.0.1',
+      description='The common used function with pytorch',
+      url='https://github.com/jiayangshi/pcf',
+      author='Jiayang Shi',
+      author_email='j.shi@liacs.leidenuniv.nl',
+      license='MIT',
+      packages=['pcf'],
+      zip_safe=False)
