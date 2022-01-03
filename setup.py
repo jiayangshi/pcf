23f517e3c6dfc2281381b0b69ac5d8e2eb029767
Jiayang <shijiayang@gmx.de>
2022-01-03 21:24:56 +0100

    change the package name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(python_requires='>=3.7',
-      name='pcf',
+      name='pcfv',
       version='0.0.1',
-      description='The common used function with pytorch',
+      description='The common used function with pytorch for vision taks',
       url='https://github.com/jiayangshi/pcf',
       author='Jiayang Shi',
       author_email='j.shi@liacs.leidenuniv.nl',
       license='MIT',
-      packages=['pcf'],
+      packages=['pcfv'],
       install_requires=[
           'pytorch >= 1.8.0',
           'numpy',
