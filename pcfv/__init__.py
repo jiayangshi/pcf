9d51e154257be587ab177de2b242e326f58f7c98
Jiayang <shijiayang@gmx.de>
2022-01-04 11:07:21 +0100

    add poisson noise function

--- pcfv/__init__.py
+++ pcfv/__init__.py
@@ -4,3 +4,4 @@ from .dataset import CustomImageDataset, CustomGreyImageDataset
 from .metric import psnr, psnr_np
 from .train import train_loop, test_loop, valid_loop
 from .utils import count_parameters
+from .noise import add_possion_noise
