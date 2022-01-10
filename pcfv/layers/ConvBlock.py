e314f79b877cbe91f7c49084ca2ba6249192a55d
shij3 <j.shi@liacs.leidenuniv.nl>
2022-01-10 16:27:54 +0100

    add conv3d option for UNet

--- pcfv/layers/ConvBlock.py
+++ pcfv/layers/ConvBlock.py
@@ -1,15 +1,21 @@
 import torch.nn as nn
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, conv3d=False):
         super(ConvBlock, self).__init__()
+        if conv3d:
+            Conv =  nn.Conv3d
+            BatchNorm = nn.BatchNorm3d
+        else:
+            Conv = nn.Conv2d
+            BatchNorm = nn.BatchNorm2d
 
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3,3), padding=(1,1))
-        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=(3,3), padding=(1,1))
+        self.conv1 = Conv(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1)
+        self.conv2 = Conv(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1)
         self.relu1 = nn.ReLU(inplace=True)
         self.relu2 = nn.ReLU(inplace=True)
-        self.batch_norm1 = nn.BatchNorm2d(num_features=out_channels)
-        self.batch_norm2 = nn.BatchNorm2d(num_features=out_channels)
+        self.batch_norm1 = BatchNorm(num_features=out_channels)
+        self.batch_norm2 = BatchNorm(num_features=out_channels)
 
     def forward(self, x):
         tmp = self.relu1(self.batch_norm1(self.conv1(x)))
