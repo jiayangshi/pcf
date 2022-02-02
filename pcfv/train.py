cd652e37e5df9473554fde27bb7a9af6956eb9ed
Jiayang <shijiayang@gmx.de>
2022-02-03 00:07:05 +0100

    fix error

--- pcfv/train.py
+++ pcfv/train.py
@@ -172,7 +172,7 @@ def early_stopping(valid_losses, patience=4):
         # if current loss larger than max value in patience
         # or last patience number losses non decreasing
         if valid_losses[-1] > max(valid_losses[-patience-1:-1]) or \
-                all(x<=y for x, y in zip(valid_losses, valid_losses[-patience-1:-1])):
+                all(x<=y for x, y in zip(valid_losses[-patience-1:-1], valid_losses[-patience:])):
             return True
     return False
 
